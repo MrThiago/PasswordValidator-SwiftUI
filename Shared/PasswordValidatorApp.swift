da9e4240609dc45a589f61b42dac2a7acf2fba92
Thiago Gouvea <ThiagoGouvea@tfl.gov.uk>
2021-10-29 22:13:41 +0100

    Initial Commit

---
+++ Shared/PasswordValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordValidatorApp.swift
+//  Shared
+//
+//  Created by Thiago Gouvea on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PasswordValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
